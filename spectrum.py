44491e19707e4e6ff5e0c13b30516f49e3451ac2
asirota12 <asirota@bu.edu>
2017-02-17 18:03:31 -0500

    put in plots, added dif matrix solutions
    also added a skip ability

--- spectrum.py
+++ spectrum.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 def alpha(p):
     return 1/np.sqrt(1/p-1)
 
-def lmbda(j,k,N,p):
+def lmbdaD(j,k,N,p):
     if j==0:
        return k/np.sqrt(N)+np.sqrt(N)*alpha(p)
     else:
@@ -20,7 +20,7 @@ def lmbda(j,k,N,p):
 def GD(q,k,N,p): #our spectrum
     s=0;
     for j in range (0,N-1):
-        s=s+1/(q-lmbda(j,k,N,p))
+        s=s+1/(q-lmbdaD(j,k,N,p))
     return s
 
 def A(p,N):
@@ -31,7 +31,7 @@ def A(p,N):
                 a[i,j]=(1/p-1)/np.sqrt(N)
             else:            
                 a[i,j]=-1*alpha(p)/np.sqrt(N)
-    print(a)
+    #print(a)
     return a;
 
 def C(k,N):
@@ -52,21 +52,21 @@ def J(N):
 
 def D(gamma,p,k,N):
     d= (gamma*C(k,N)+alpha(p)*J(N))/np.sqrt(N)
-    print(d)
+    #print(d)
     return d
     
 def S(gamma,p,N,k):
     s=A(p,N)+D(gamma,p,k,N)
-    print(s)
+   # print(s)
     return s
 
-def plot(re,im,b,N,p,k):
+def plot(re,im,b,N,p,k,mat):
     plt.figure(1)
     plt.subplot(211)
     plt.hist2d(re, im, bins=b,norm=LogNorm())
     plt.colorbar()
-    title='Eigenvalues of S,N={0},p={1},k={2}'.format(N,p,k)
-    filename='hist({0}N)({1}p)({2}k).png'.format(N,p,k)
+    title='Eigenvalues of {3},N={0},p={1},k={2}'.format(N,p,k,mat)
+    filename='hist({0}=N)({1}=p)({2}=k)({3}=mat).png'.format(N,p,k,mat)
     plt.title(title)
     plt.xlabel('real(lambda)')
     plt.ylabel('imag(lambda)')
@@ -74,28 +74,74 @@ def plot(re,im,b,N,p,k):
     
     plt.figure(2)
     plt.subplot(212)
-    plt.plot(re, im,'bo',markersize=1)
+    plt.plot(re, im,'bo',markersize=5)
     plt.title(title)
     plt.xlabel('real(lambda)')
     plt.ylabel('imag(lambda)')
     
-    filename='plot({0}N)({1}p)({2}k).png'.format(N,p,k)
+    filename='plot({0}=N)({1}=p)({2}=k)({3}=mat).png'.format(N,p,k,mat)
     plt.savefig(filename, bbox_inches='tight')
     plt.close('all')
-   # print("plotted")
+    # print("plotted")
     return
 
-def run(k,p,N,gamma,b,runs):
-    re=[]
-    im=[]
+def solveD(gamma,p,N,k,b,runs,skip=False):
+    reD=[]
+    imD=[]
     for i in range(runs):
         if (i%10==0):
             print i/1000.0 
-        eigvals=np.linalg.eigvals(S(gamma,p,N,k))
-        for z in eigvals:
-            re.append(z.real)
-            im.append(z.imag)
-    plot(re,im,b,N,p,k)
+        eigvalD=np.linalg.eigvals(D(gamma,p,N,k))
+        for z in eigvalD:
+            if(skip and z.real>3):
+                print('skipped in D')
+            else:
+                reD.append(z.real)
+                imD.append(z.imag)
+    plot(reD,imD,b,N,p,k,'D')
+
+def solveA(gamma,p,N,k,b,runs,skip=False):
+    reA=[]
+    imA=[]
+    for i in range(runs):
+        if (i%10==0):
+            print i/1000.0 
+        eigvalA=np.linalg.eigvals(A(p,N))
+        for z in eigvalA:
+            if(skip and z.real>3):
+                print('skipped in A')
+            else:
+                reA.append(z.real)
+                imA.append(z.imag)
+    plot(reA,imA,b,N,p,k,'A')
+    
+def solveS(gamma,p,N,k,b,runs,skip=False):
+    reS=[]
+    imS=[]
+    for i in range(runs):
+        if (i%10==0):
+            print i/1000.0 
+        eigvalS=np.linalg.eigvals(S(gamma,p,N,k))
+        for z in eigvalS:
+            if(skip and z.real>3):
+                print('skipped in S')
+            else:
+                reS.append(z.real)
+                imS.append(z.imag)
+    plot(reS,imS,b,N,p,k,'S')
+    
+def solveDequ(p,N,k,b,runs,skip=False): 
+    reDequ=[]
+    imDequ=[]  
+    x=0
+    if skip:
+        x=1
+    for i in range(x,N): #skipping number 0
+        z=lmbdaD(i,k,N,p)
+        reDequ.append(z.real)
+        imDequ.append(z.imag)
+    plot(reDequ,imDequ,b,N,p,k,'Dequ')
+
 
 """
 k is connectivity its even
@@ -105,13 +151,16 @@ b is bins
 """  
 k=4
 p=.2 
-N=100
+N=1000
 gamma=1
-b=200
+b=20
 runs=1
 
 #for j in range(2,1000,100):
  #   for i in range(4):
-run(k,p,N,gamma,b,runs)
+solveD(gamma,p,N,k,b,runs)
+solveA(gamma,p,N,k,b,runs)
+solveS(gamma,p,N,k,b,runs)
+solveDequ(p,N,k,b,runs)
 
 
